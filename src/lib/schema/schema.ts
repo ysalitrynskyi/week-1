de7a83c21672695bf6b1e342ee89e4f4a8eac397
Yevhen Salitrynskyi <ysalitrynskyi@gmail.com>
2025-01-15 09:27:18 -0500

    Dev

--- src/lib/schema/schema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const formSchema = z.object({
-	email: z.string().email(),
-});
-
-export type FormSchema = typeof formSchema;
--- src/routes/(app)/(auth)/+layout@.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	import { toast, Toaster } from 'svelte-sonner';
-</script>
-
-<Toaster theme='dark' />
-<slot></slot>
